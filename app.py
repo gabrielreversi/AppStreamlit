d448b28c0bf9116f0e35651bd6a0fc51cb917c69
Gabriel <gabriel_reversi@hotmail.com>
2024-10-13 19:28:59 -0300

    Implementando rotas CRUD

--- app.py
+++ app.py
@@ -1,11 +1,54 @@
 
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from schemas import Message, UserDB, UserList, UserPublic, UserSchemas
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', status_code=HTTPStatus.OK)
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'hello world!'}
+
+
+@app.post('/user/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchemas):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchemas):
+    if user_id > len(database) or user_id < 0:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
